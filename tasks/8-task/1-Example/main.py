cd3be7cba91414740a7ceaf7c9195f8ec330422c
leomerida15 <dimasmerida15@gmail.com>
2022-11-10 17:33:37 -0400

    8-task ok

--- tasks/8-task/1-Example/main.py
+++ tasks/8-task/1-Example/main.py
@@ -1,9 +1,9 @@
-f = open('./tasks/8-task/1-Example/hola.txt', 'w')
+f = open('./tasks/8-task/1-Example/hola.txt', 'x')
 
 f.close()
 
-file = open('./tasks/8-task/1-Example/hola.txt', 'w')
+# file = open('./tasks/8-task/1-Example/hola.txt', 'w')
 
-file.write("Hola")
+# file.write("Hola")
 
-file.close()
+# file.close()
